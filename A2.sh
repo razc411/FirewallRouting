914272f5c93086dcb3693b55f6f95b3986133031
root <root@Raz-LT.raz>
2015-02-02 18:55:58 -0800

    Modified the tcp/udp socket handling. Also tried to straighten out the commands and moved the nat commands to the network script.

--- A2.sh
+++ A2.sh
@@ -12,8 +12,8 @@
 #                          USER CONFIGUREABLE SECTION                                   #
 #########################################################################################
 
-ALLOWED_TCP_PORTS="89,86,88,1000:4000"
-ALLOWED_UDP_PORTS="89,86,88,1000:4000"
+ALLOWED_TCP_PORTS="89,86,88,443,80"
+ALLOWED_UDP_PORTS="89,86,88,443,80"
 ALLOWED_ICMP_PACKET_TYPES="1,2,3"
 INTERNAL_FIREWALL_HOST="192.168.10.1"
 EXTERNAL_FIREWALL_IP="192.168.0.17"
@@ -35,10 +35,6 @@ $FW_PROGRAM_DIR$FW_NAME -P INPUT DROP
 $FW_PROGRAM_DIR$FW_NAME -P FORWARD DROP
 $FW_PROGRAM_DIR$FW_NAME -P OUTPUT DROP
 
-#NAT routing rules
-$FW_PROGRAM_DIR$FW_NAME -t nat -A POSTROUTING -s $SUBNET_ADDR -o em1 -j SNAT --to-source $EXTERNAL_FIREWALL_IP
-$FW_PROGRAM_DIR$FW_NAME -t nat -A PREROUTING -i em1 -j DNAT --to-destination 192.168.10.2
-
 #Block external maliciousness
 $FW_PROGRAM_DIR$FW_NAME -A INPUT -i $EXTERNAL_INTERFACE -d $INTERNAL_FIREWALL_HOST -j DROP
 $FW_PROGRAM_DIR$FW_NAME -A INPUT -i $EXTERNAL_INTERFACE -s 192.168.10.0/24 -j DROP
@@ -47,22 +43,23 @@ $FW_PROGRAM_DIR$FW_NAME -A INPUT -i $EXTERNAL_INTERFACE -p tcp -m multiport --dp
 #Drop all TCP packets with the SYN and FIN bit set
 $FW_PROGRAM_DIR$FW_NAME -A INPUT -p tcp --tcp-flags ALL SYN,FIN -j DROP
 
-#Create services and its join
-$FW_PROGRAM_DIR$FW_NAME -N services
-$FW_PROGRAM_DIR$FW_NAME -A INPUT -j services
-$FW_PROGRAM_DIR$FW_NAME -A OUTPUT -j services
-
 #Do not allow Telnet packets at all
-$FW_PROGRAM_DIR$FW_NAME -A services -p tcp --dport 23 -j DROP
-$FW_PROGRAM_DIR$FW_NAME -A services -p tcp --sport 23 -j DROP
+$FW_PROGRAM_DIR$FW_NAME -A FORWARD -p tcp --dport 23 -j DROP
+$FW_PROGRAM_DIR$FW_NAME -A FORWARD -p tcp --sport 23 -j DROP
 
 #Inbound/Outbound TCP packets on allowed ports
-$FW_PROGRAM_DIR$FW_NAME -A services -p tcp -m state --state ESTABLISHED,NEW -m multiport --dports $ALLOWED_TCP_PORTS -j ACCEPT
-$FW_PROGRAM_DIR$FW_NAME -A services -p tcp -m state --state ESTABLISHED,NEW -m multiport --sports $ALLOWED_TCP_PORTS -j ACCEPT 
+arr=$(echo $ALLOWED_TCP_PORTS | tr "," "\n")
+for PORT in $arr
+	do $FW_PROGRAM_DIR$FW_NAME -A FORWARD -p tcp -m state --state ESTABLISHED,NEW --dport $PORT -j ACCEPT
+	   $FW_PROGRAM_DIR$FW_NAME -A FORWARD -p tcp -m state --state ESTABLISHED,NEW --sport $PORT -j ACCEPT 
+done
 
 #Inbound/Outbound UDP packets on allowed ports
-$FW_PROGRAM_DIR$FW_NAME -A services -p udp -m state --state ESTABLISHED,NEW -m multiport --dports $ALLOWED_UDP_PORTS -j ACCEPT
-$FW_PROGRAM_DIR$FW_NAME -A services -p udp -m state --state ESTABLISHED,NEW -m multiport --sports $ALLOWED_UDP_PORTS -j ACCEPT
+arr=$(echo $ALLOWED_UDP_PORTS | tr "," "\n")
+for PORT in $arr
+	do $FW_PROGRAM_DIR$FW_NAME -A FORWARD -p udp -m state --state ESTABLISHED,NEW --dport $PORT -j ACCEPT
+	   $FW_PROGRAM_DIR$FW_NAME -A FORWARD -p udp -m state --state ESTABLISHED,NEW --sport $PORT -j ACCEPT
+done
 
 #Allow DHCP on all adapters
 $FW_PROGRAM_DIR$FW_NAME -A INPUT -p udp -m state --state ESTABLISHED,NEW --sport 67:68 --dport 67:68 -j ACCEPT
@@ -76,14 +73,12 @@ $FW_PROGRAM_DIR$FW_NAME -A INPUT -p tcp -m state --state ESTABLISHED,NEW --sport
 #Inbound/Outbound ICMP packets based on type numbers
 arr=$(echo $ALLOWED_ICMP_PACKET_TYPES | tr "," "\n")
 for TYPE in $arr
-	do $FW_PROGRAM_DIR$FW_NAME -A services -p icmp --icmp-type $TYPE -m state --state ESTABLISHED,NEW -j ACCEPT
+	do $FW_PROGRAM_DIR$FW_NAME -A FORWARD -p icmp --icmp-type $TYPE -m state --state ESTABLISHED,NEW -j ACCEPT
 done
 
 #Accept fragments
 $FW_PROGRAM_DIR$FW_NAME -A INPUT -i $EXTERNAL_INTERFACE -f -j ACCEPT
 $FW_PROGRAM_DIR$FW_NAME -A INPUT -i $INTERNAL_INTERFACE -f -j ACCEPT
-
-#Accept all TCP packets that belong to an existing connection(on allowed ports)
 
 #Set control connections to Minimum delay for FTP and SSH services
 $FW_PROGRAM_DIR$FW_NAME -A PREROUTING -t mangle -p tcp --sport 21 -j TOS --set-tos Minimize-Delay
--- Network.sh
+++ Network.sh
@@ -1,7 +1,7 @@
 PRIMARY_INTERFACE="em1"
 SECONDARY_INTERFACE="p3p1"
 SUBNET_ADDR="192.168.10.0/24"
-
+EXTERNAL_FIREWALL_IP="192.168.0.17"
 
 PS3='Is this Firewall Host or Internal Host(Enter number): '
 select opt in Firewall Host Reset Exit
@@ -12,10 +12,7 @@ do
 			ifconfig $SECONDARY_INTERFACE 192.168.10.2 up
 			route add default gw 192.168.10.1
 
-			echo 'domain ad.bcit.ca' > /etc/resolv.conf
-			echo 'search ad.bcit.ca' > /etc/resolv.conf
-			echo 'nameserver 142.232.191.38' > /etc/resolv.conf
-			echo 'nameserver 142.232.191.39' > /etc/resolv.conf
+			echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 
 			echo "Internal Host Setup complete."
 			break
@@ -24,9 +21,14 @@ do
 		Firewall)
 			ifconfig $SECONDARY_INTERFACE 192.168.10.1 up
 			echo "1" > /proc/sys/net/ipv4/ip_forward
+			echo 'nameserver 8.8.8.8' > /etc/resolv.conf
+
 			route add -net 192.168.0.0 netmask 255.255.255.0 gw 192.168.0.100
 			route add -net $SUBNET_ADDR gw 192.168.10.1
 			
+			iptables -t nat -A POSTROUTING -s $SUBNET_ADDR -o $PRIMARY_INTERFACE -j SNAT --to-source $EXTERNAL_FIREWALL_IP
+			iptables -t nat -A PREROUTING -i $PRIMARY_INTERFACE -j DNAT --to-destination 192.168.10.2
+
 			echo "Firewall setup complete."
 			echo "Deploying Firewall Rules.."
 			chmod +x A2.sh
